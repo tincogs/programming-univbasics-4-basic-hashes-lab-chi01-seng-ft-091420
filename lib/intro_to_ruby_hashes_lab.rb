f9a24acf958929261c6b18f9b445d769aa86e7f4
Tom ODay <tomoday91@gmail.com>
2020-09-05 21:42:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,11 +11,11 @@ end
 
 def pioneer
   {
-    name: 'Grace Hopper'
+    name: "Grace Hopper"
   }
 end
 
-number = 4
+number_id { id: 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
 end
